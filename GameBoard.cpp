6f3a3f74deab6119d21d99b111ec7940a299e14b
Naglaa Eid <neid027@uottawa.ca>
2014-11-27 15:24:01 -0500

    Not really much

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -1,46 +1,46 @@
-#include <vector>
 #include "gameboard.h"
 
-// TODO test commit
+template <class T, class P, const int R, const int C>
+GameBoard(const T& tile, int row, int col){
 
-using namespace std;
+}
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 void GameBoard::add(const T& tile, int row, int col){
-
+	d_tilesVec[row][col] = tile;
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 const T& getTile(int row, int col) const{
-
+	return d_tilesVec[row][col];
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 void getCoordinate(const T &tile, int *row, int *col) const{
 
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 void setPlayer(J player){
 
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 J getPlayer(const string& playerName){
 
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 const T& getTile(const string& playerName) const{
 
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 vector<J> getPlayers(const T& tile) const{
 
 }
 
-template <class T, class P, const int N>
+template <class T, class P, const int R, const int C>
 const T& move(Enum Move move, const string& playerName){
 
 }
--- gameboard.h
+++ gameboard.h
@@ -1,14 +1,47 @@
 #include <iostream>
+#include <map>
+#include <vector>
 
-template <class T, class P, const int N>
+using std::map;
+using std::string;
+using std::vector;
+
+template <class T, class J, const int R, const int C>
 class GameBoard {
+	// TODO might not need this
+	int d_numPlayers;
+	vector<J> d_players;
+
+	// TODO would be useful to use boost::multi_index's bidirectional map
+	vector<vector<vector<J>>> d_tilesVec;
+	// OR
+	map<T, vector<J>> d_tilesMap;
+
+	enum Move {
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	};
+
 public:
+	GameBoard();
+
+	GameBoard(vector<J> players);
+
 	void add(const T& tile, int row, int col);
+
 	const T& getTile(int row, int col) const;
+
 	void getCoordinate(const T &tile, int *row, int *col) const;
+
 	void setPlayer(J player);
-	J getPlayer(const std::string& playerName);
-	const T& getTile(const std::string& playerName) const;
-	std::vector<J> getPlayers(const T& tile) const;
-	const T& move(Enum Move move, const std::string& playerName);
+
+	J getPlayer(const string& playerName);
+
+	const T& getTile(const string& playerName) const;
+
+	vector<J> getPlayers(const T& tile) const;
+
+	const T& move(Move move, const string& playerName);
 };
